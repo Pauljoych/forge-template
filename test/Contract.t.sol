546625303379d9a3d4e195402886b1ee6eebde21
Pauljoych <luthfiquart@gmail.com>
2022-04-23 14:58:16 +0700

    init

---
+++ test/Contract.t.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: Unlicense
+pragma solidity ^0.8.0;
+
+import "forge-std/Test.sol";
+
+import "src/Contract.sol";
+
+contract TestContract is Test {
+    Contract c;
+
+    function setUp() public {
+        c = new Contract();
+    }
+}
